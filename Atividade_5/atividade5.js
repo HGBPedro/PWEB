0e77ebe953539d77d28dc374edc49fc988a1d823
pentrU <pedro.bonel@gmail.com>
2020-05-11 12:31:09 -0300

    Atividade 5 de pweb

---
+++ Atividade_5/atividade5.js
@@ -0,0 +1,15 @@
+function maior (){
+    var n1 = prompt("Digite o primeiro numero:");
+    var n2 = prompt("Digite o segundo numero:");
+    var n3 = prompt("Digite o terceiro numero:");
+
+    if(n1 > n2 && n1 > n3){
+        alert("O primeiro número (" + n1 + ") é o maior");
+    } else if (n2 > n1 && n2 > n3) {
+        alert("O segundo número (" + n2 + ") é o maior");
+    } else if (n3 > n2 && n3 > n1) {
+        alert("O terceiro número (" + n3 + ") é o maior");
+    } else if ( n1 == n2 || n1 == n3 || n2 == n3){
+        alert("Os maiores números são iguais")
+    }
+}
