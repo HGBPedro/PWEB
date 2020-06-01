6d8d96d2ef8400c2b434828565c4705ca560771a
pentrU <pedro.bonel@gmail.com>
2020-06-01 09:08:29 -0300

    Atividades 7 e 8 de PWEB

---
+++ Atividade_7/atividade7.js
@@ -0,0 +1,11 @@
+function atividadeA () {
+    alert("Atividade A:" + "\n" +"Insira os lados do reângulo:");
+    var x = prompt("Largura");
+    var y = prompt("Altura");
+
+    alert("A área do triângulo é: " + x*y);
+}
+
+function atividadeB () {
+    
+}
---
+++ Atividade_8/script.js
@@ -0,0 +1,11 @@
+function janelaAberta () {
+   document.getElementById("janela").src = "./janelas2.jpg"
+}
+
+function janelaFechada() {
+    document.getElementById("janela").src = "./janelas.jpg"
+}
+
+function janelaQuebrada() {
+    document.getElementById("janela").src = "./janelas3.jpg"
+}
